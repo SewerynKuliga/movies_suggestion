c6b1b30c0ad5a22377f57a2ba59d430103d0a340
Seweryn Kuliga <skuligas@gmail.com>
2021-11-04 16:12:59 +0100

    Update

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Main() {
   const [movies, setMovies] = useState(null);
@@ -16,19 +20,56 @@ function Main() {
 
   return (
     <section>
-      {movies &&
-        movies.map((movie) => (
-          <div key={movie.id}>
-            <h1>{movie.title}</h1> <h2>({movie.rating}/10)</h2>
-            <img src={movie.imageURL} alt="obrazek tytułowy"/>
-            <div>
-              <button>Accept</button>
-              <button>Reject</button>
-            </div>
-          </div>
-        ))}
+      <Slider speed={500} slidesToShow={1} slidesToScroll={1} infinite={true}>
+        {movies &&
+          movies.map((movie) => (
+            <Body key={movie.id}>
+              <Title>
+                {movie.title} ({movie.rating}/10)
+              </Title>
+              <Img src={movie.imageURL} alt="obrazek tytułowy" />
+              <BtnsField>
+                <Button style={{backgroundColor: "rgb(10,198,21)"}}>Accept</Button>
+                <Button style={{backgroundColor: "rgb(215,2,2)"}}>Reject</Button>
+              </BtnsField>
+            </Body>
+          ))}
+      </Slider>
     </section>
   );
 }
 
 export default Main;
+
+const Body = styled.div`
+  height: 97vh;
+  text-align: center;
+`;
+const Title = styled.h1`
+  margin-top: 2rem;
+  font-size: 4rem;
+`;
+const Img = styled.img`
+  height: 70%;
+  margin: 2rem auto;
+  border-radius: 0.3rem;
+`;
+const BtnsField = styled.div`
+  width: 40rem;
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem auto;
+`;
+const Button = styled.div`
+  width: 10rem;
+  padding: 1.5rem;
+  font-size: 2rem;
+  border-radius: 3rem;
+  transition: 0.3s;
+  cursor: pointer;
+  
+  &:hover{
+    border-radius: 1rem;
+
+  }
+`;
